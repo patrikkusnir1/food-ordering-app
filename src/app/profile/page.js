7791e030f5108a26358c16a2cd77c1963cf18924
Patrik Kusnir <patrikkusnir1@gmail.com>
2024-03-06 13:45:24 +0300

    added panel profile saved and saving

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -8,6 +8,8 @@ import { useEffect, useState } from "react";
 export default function ProfilePage() {
     const session = useSession();
     const [userName, setUserName] = useState("");
+    const [saved, setSaved] = useState(false);
+    const [isSaving, setIsSaving] = useState(false);
     const { status } = session;
 
     useEffect(() => {
@@ -20,14 +22,18 @@ export default function ProfilePage() {
 
     async function handleProfileInfoUpdate(ev) {
         ev.preventDefault();
+        setSaved(false);
+        setIsSaving(true);
         const response = await fetch("/api/profile", {
             method: "PUT",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ name: userName })
         });
-
+        setIsSaving(false)
+        if (response.ok) {
+            setSaved(true);
+        }
     }
-
     if (status === "loading") {
         return "Loading..."
     }
@@ -43,6 +49,19 @@ export default function ProfilePage() {
                 Profile
             </h1>
             <div className="max-w-md mx-auto">
+                {/* { if saved - show profile saved } */}
+                {saved && (
+                    <h2 className="text-center bg-green-100 p-4 rounded-lg border border-green-300">
+                        Profile saved!
+                    </h2>
+
+                )}
+                {isSaving && (
+                    <h2 className="text-center bg-blue-100 p-4 rounded-lg border border-blue-300">
+                        Saving...
+                    </h2>
+                )}
+
                 <div className="flex gap-4 items-center">
                     <div className="p-2 rounded-lg relative">
                         <Image className="rounded-lg w-full h-full mb-1" src={userImage} width={250} height={250} alt={"avatar"} />
